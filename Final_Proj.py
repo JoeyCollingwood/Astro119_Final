75a7fa290c01b031138a6a143dff6a442fbd6747
JoeyCollingwood <51378730+JoeyCollingwood@users.noreply.github.com>
2019-06-04 10:36:35 -0700

    Update Final_Proj.py

--- Final_Proj.py
+++ Final_Proj.py
@@ -3,3 +3,18 @@
 Final project question 3(b)
 Group members: Joey C & Luke W
 """
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+data_dir = 'C:\\Users\\Joey\\Desktop\\aapython\\Final'
+star_data = 'hygdata_v3.csv'
+
+#===========================LOAD DATA================================
+os.chdir(data_dir)
+
+stars = np.loadtxt(star_data).T
+
+
+#===========================PLOTTING=================================
