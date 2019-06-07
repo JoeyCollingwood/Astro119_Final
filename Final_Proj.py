ff64349c47ef306b6da64bce00d1e436ee7ad569
JoeyCollingwood <51378730+JoeyCollingwood@users.noreply.github.com>
2019-06-07 10:02:37 -0700

    Update Final_Proj.py

--- Final_Proj.py
+++ Final_Proj.py
@@ -1,20 +1,97 @@
 # -*- coding: utf-8 -*-
 """
-Final project question 3(b)
-Group members: Joey C & Luke W
+Created on Thu Jun  6 15:37:18 2019
+
+@author: Joey
 """
 
-import os
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#from mpl_toolkits import mplot3d
+#from mpl_toolkits.mplot3d import Axes3D
 
-data_dir = 'C:\\Users\\Joey\\Desktop\\aapython\\Final'
-star_data = 'hygdata_v3.csv'
+file_in = 'hygdata_v3.csv'
+df = pd.read_csv(file_in)
+mData = np.genfromtxt(file_in, dtype=None, delimiter = ',',skip_header = 1, usecols=[9,12,16,17,18,19,33]
+                      ).T
 
-#===========================LOAD DATA================================
-os.chdir(data_dir)
-stars = np.loadtxt(star_data).T
+median = df['ci'].median()
+df['ci'].fillna(method='backfill', inplace=True)
+#print(df.isnull().sum()['ci'])
 
-#===========================PLOTTING=================================
-plt.figure(1)
-ax = plt.subplot(111)
+ci = df[['ci']].values  #using pandas to replace NaN values in ci column, then replacing them with backfilled values
+dist   = mData[0]
+radvel = mData[1]
+color  = mData[2]
+x_pos = mData[3]
+y_pos = mData[4]
+z_pos = mData[5]
+lumin  = mData[6]
+
+t_sun = 5800
+l_sun = 3.85 * 10**26
+temp    = []
+radius  = []
+lum_new = []
+for i in range (len(ci)):
+    
+    t = 4600*((1/(.92*ci[i]+1.7))+1/(.92*ci[i]+.62))
+    r = np.sqrt(((lumin[i]) * (l_sun)) /(4*np.pi*(5.67*(10**(-27))*t**4)))
+    temp.append(t/t_sun)
+    radius.append(r)
+    lum_new.append(lumin[i])
+
+#def temp(ci):
+#    return 4600 * ((1/((.92*ci)+1.7))+(1/((.92*ci)+0.62)))
+#def radius(lumin):
+#    return np.sqrt(((lumin) * (l_sun)) /(4*np.pi*(5.67*(10**(-27))*temp(ci)**4)))
+
+def lin_LS( aX, aY):
+    """
+    - linear least squares assuming normal distributed errors in Y, no errors in X
+    :param aX: - independent variable
+    :param aY: - measured dependent variable
+    :return: float(<slope>)
+    """
+    meanX = aX.mean()
+    meanY = aY.mean()
+    # variance and co-variance - 1./N term omitted because it cancels in the following ratio
+    VarX  = ( (aX - meanX)**2).sum()
+    #VarY  = ( (aY - meanY)**2).sum()
+    CovXY = ( (aY-meanY)*(aX-meanX)).sum()
+    slope = CovXY/VarX
+    a     = meanY - meanX*slope
+    return slope, a
+
+
+log_lum  = np.log10(lumin)            #log luminosity in solar units
+log_temp = np.log10(temp)   #log temperature in solar units
+
+slope,f_a = lin_LS(log_lum[0:1000],log_temp[0:1000])
+#R_fit = np.linspace(np.median(radius)-1e10,np.median(radius)+1e10,100)
+aPLfit = 10**(f_a)*temp[::]**slope
+print(slope)
+#print(aPLfit)
+
+def func(r, alpha, t, beta):
+    return (r**alpha)*t**beta
+
+#fig1 = plt.figure(1)
+#plt.scatter(log_temp,log_lum,s=.05,c = 'black')
+#plt.gca().invert_xaxis()
+#plt.title('Hertzsprung-Russell Diagram')
+#plt.xlabel('Temperature')
+#plt.ylabel('Luminosity')
+#plt.grid(True, linestyle = 'dotted', c = 'black')
+#fig2 = plt.figure(2)
+#plt.loglog(temp,aPLfit,c = 'r')
+
+#==============================3D PLOTTING=====================================
+fig2 = plt.figure(2)
+ax = fig2.add_subplot(111,projection='3d')
+ax.scatter3D(x_pos,y_pos,z_pos,c=color,s=np.exp(color))
+cbar = plt.colorbar(ax, orientation = 'horizontal')
+cbar.set_label("Color Index")
+#c = ax.colorbar(ax, orientation = 'horizontal')
+#c.set_label('Color Index')
