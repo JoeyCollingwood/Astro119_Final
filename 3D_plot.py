584063befaea01cb5f347429f3f25af9e2da05e0
lukewaldschmidt <49295960+lukewaldschmidt@users.noreply.github.com>
2019-06-12 17:04:23 -0700

    Update 3D_plot.py

--- 3D_plot.py
+++ 3D_plot.py
@@ -4,16 +4,19 @@ Created on Thu Jun  6 15:37:18 2019
 @author: Joey
 """
 
+#=================modules/packages===============================
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_in = 'data/hygdata_v3.csv'
+#==================reading in data file==========================
+file_in = 'hygdata_v3.csv'
 df = pd.read_csv(file_in)
 mData = np.genfromtxt(file_in, dtype=None, delimiter = ',',skip_header = 1, usecols=[9,12,16,17,18,19,33]
                       ).T
 
-median = df['ci'].median()
+#====================filling in missing values=====================
 df['ci'].fillna(method='backfill', inplace=True)
 #print(df.isnull().sum()['ci'])
 
@@ -26,6 +29,8 @@ y_pos = mData[4]
 z_pos = mData[5]
 lumin  = mData[6]
 
+
+#====================calculating temp/radius========================
 t_sun = 5800
 l_sun = 3.85 * 10**26
 temp    = []
@@ -41,14 +46,13 @@ for i in range (len(ci)):
 
 
 #==============================3D PLOTTING=====================================
-fig2 = plt.figure(2,figsize=[8,8])
-ax = fig2.add_subplot(111,projection='3d')
+fig = plt.figure(1,figsize=[8,8])
+ax = fig.add_subplot(111,projection='3d')
 plot = ax.scatter(x_pos[0::10],y_pos[0::10],z_pos[0::10],c=np.reshape(ci,119614)[0::10],s=4,cmap='RdYlBu',vmin=0,vmax=1.5)
 cbar = plt.colorbar(plot, orientation = 'horizontal')
 cbar.set_label("Color Index (B-V)")
-ax.set_xticks(0,0)
-ax.set_yticks(0,0)
-ax.set_zticks(0,0)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 plt.title('3D position')
 plt.show()
-
